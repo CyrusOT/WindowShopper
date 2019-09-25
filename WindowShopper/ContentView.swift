0507669ee5da4857ce1a518db0303c35f12ecfc8
AhmedOT <iachmed@me.com>
2019-09-24 19:22:01 -0700

    Initial Commit

---
+++ WindowShopper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WindowShopper
+//
+//  Created by Ahmed AlOtaibi on 9/24/19.
+//  Copyright © 2019 Ahmed AlOtaibi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
